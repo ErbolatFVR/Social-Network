6ed0a6370c5e8bb6a3d3feedbbe05eb533af128a
ErbolatFVR <erbolaterkinbek@gmail.com>
2020-10-30 22:51:17 +0600

    Добавил свой проект на GIT

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/471px-Google_%22G%22_Logo.svg.png'></img>
+    </header>
+}
+
+
+
+export default Header;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+    <div>
+
+    </div>
+    <div>
+      <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/471px-Google_%22G%22_Logo.svg.png'></img>
+    </div>
+    <div>
+      ava + description
+    </div>
+    <div>
+      my posts
+     <div>
+        new post
+       <div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+}
+
+
+export default Profile;
